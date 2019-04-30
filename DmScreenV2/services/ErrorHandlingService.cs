f5bd30fcc121fc962e7abc283af904af9bcd37c6
Bartosz <bartosz_@live.ca>
2019-04-29 21:16:17 -0400

    init of error handling and http request services

---
+++ DmScreenV2/services/ErrorHandlingService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.IO;
+
+namespace DmScreenV2.services
+{
+    public class ErrorHandlingService
+    {
+        private static string CurrentWorkingDirectory = ConfigurationSettings.AppSettings.Get("DefaultSaveLocation") + @"\resources\log.txt";
+
+
+        //
+        //Writes the specified error message to the error log.
+        //
+        public static void WriteToLog(string errorMsg)
+        {
+            using (StreamWriter stream = new StreamWriter(CurrentWorkingDirectory))
+            {
+                stream.Write(errorMsg);
+            }
+        }
+    }
+}
---
+++ DmScreenV2/services/HttpService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace DmScreenV2.services
+{
+    class HttpService
+    {
+        private const string API_BASE_URL = "http://dnd5eapi.co/api/";
+
+        public static void GetAllMonsters()
+        {
+            try
+            {
+                WebRequest request = WebRequest.Create(API_BASE_URL + "monsters/");
+                WebResponse response = request.GetResponse();
+                //do other stuff lol
+                //https://docs.microsoft.com/en-us/dotnet/framework/network-programming/how-to-request-data-using-the-webrequest-class
+                //follow the above link
+            }
+            catch (Exception e)
+            {
+                //implement a specific request? figure out what error to catch?
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+    
+        //
+        //Enter the keyword for monsters and it returns a list sorted in order of search results;
+        //order is decreasing similarity to the searched word.
+        //
+        public static void SearchMonsterList(string searchWord)
+        {
+            //TO DO: change from void to something else, amongst other things.
+        }
+
+
+
+    }
+}
